e0eae0a4e7471543304a532ec390c4f6489c2dbb
oscar.dyremyhr@gmail.com <oscar.dyremyhr@gmail.com>
2023-03-23 11:45:11 +0000

    New files - template

---
+++ Medallion Architecture/02_Silver/silver_notebook_template.py
@@ -0,0 +1,55 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # Silver - {Table}
+# MAGIC 
+# MAGIC Overview:
+# MAGIC - Loads and transforms bronze data from X to the X
+# MAGIC 
+# MAGIC 
+# MAGIC ---
+# MAGIC 
+# MAGIC |   **Configuration**   | **Option**    |
+# MAGIC | --------|---------|
+# MAGIC | **Author**  | NAME |
+# MAGIC | **Date created** | DATE |
+# MAGIC | **Date updated** | - |
+# MAGIC | **Configuration path** | ../../00_Config/common_config |
+# MAGIC | **Data Load path** | abfss://{container}@{storageaccount}.dfs.core.windows.net/ |
+# MAGIC | **Data save path** | abfss://{container}@{storageaccount}.dfs.core.windows.net/ |
+# MAGIC | **Catalog** | X |
+# MAGIC | **Database** | silver |
+# MAGIC | **Table format** | Delta |
+# MAGIC | **Delta table prefix** | X |
+# MAGIC 
+# MAGIC ----
+# MAGIC 
+# MAGIC Links to documentation:  
+# MAGIC [DevOps - X](https://x.com)
+# MAGIC 
+# MAGIC 
+# MAGIC ## History
+# MAGIC 
+# MAGIC  
+# MAGIC 
+# MAGIC | Date | Developed By | Reason |
+# MAGIC |:----:|--------------|--------|
+# MAGIC | xxxx-xx-xx   | X| Notebook created      |
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Extract
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Transform
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Load
